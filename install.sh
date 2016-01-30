8025d1e539845b46cac7d7f82c882f8731d16570
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2016-01-31 05:22:17 +0800

    fix install script

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
-#!bin/bash
+#!/bin/bash
 
 mkdir bundle
+cd bundle
 
-git clone https://github.com/moll/vim-bbye.git bundle/
-git clone https://github.com/kien/ctrlp.vim.git bundle/
-git clone https://github.com/Raimondi/delimitMate.git bundle/
-git clone https://github.com/othree/es.next.syntax.vim.git bundle/
-git clone https://github.com/Yggdroot/indentLine.git bundle/
-git clone https://github.com/Shougo/neocomplete.vim.git bundle/
-git clone https://github.com/scrooloose/nerdcommenter.git bundle/
-git clone https://github.com/scrooloose/nerdtree.git bundle/
-git clone https://github.com/scrooloose/syntastic.git bundle/
-git clone https://github.com/godlygeek/tabular.git bundle/
-git clone https://github.com/vim-scripts/taglist.vim.git bundle/
-git clone https://github.com/vim-airline/vim-airline.git bundle/
-git clone https://github.com/Chiel92/vim-autoformat.git bundle/
-git clone https://github.com/airblade/vim-gitgutter.git bundle/
-git clone https://github.com/mxw/vim-jsx.git bundle/
-git clone https://github.com/terryma/vim-multiple-cursors.git bundle/
-git clone https://github.com/myhere/vim-nodejs-complete.git bundle/
-git clone https://github.com/honza/vim-snippets.git bundle/
-git clone https://github.com/othree/yajs.vim.git bundle/
+git clone https://github.com/moll/vim-bbye.git 
+git clone https://github.com/kien/ctrlp.vim.git 
+git clone https://github.com/Raimondi/delimitMate.git 
+git clone https://github.com/othree/es.next.syntax.vim.git 
+git clone https://github.com/Yggdroot/indentLine.git 
+git clone https://github.com/Shougo/neocomplete.vim.git 
+git clone https://github.com/scrooloose/nerdcommenter.git 
+git clone https://github.com/scrooloose/nerdtree.git 
+git clone https://github.com/scrooloose/syntastic.git 
+git clone https://github.com/godlygeek/tabular.git 
+git clone https://github.com/vim-scripts/taglist.vim.git 
+git clone https://github.com/vim-airline/vim-airline.git 
+git clone https://github.com/Chiel92/vim-autoformat.git 
+git clone https://github.com/airblade/vim-gitgutter.git 
+git clone https://github.com/mxw/vim-jsx.git 
+git clone https://github.com/terryma/vim-multiple-cursors.git 
+git clone https://github.com/myhere/vim-nodejs-complete.git 
+git clone https://github.com/honza/vim-snippets.git 
+git clone https://github.com/othree/yajs.vim.git 
