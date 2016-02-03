168ed83941c4b5af7ee0876f6ee033a8f4e63d88
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2016-02-03 16:41:49 +0800

    upgrade pathogen to 2.4

--- update.sh
+++ update.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# update Pathogen
+echo "START updating Pathogen ..."
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "DONE. "
+echo " "
+
+# git pull all submodule 
 for d in bundle/*/ ; 
 do 
     cd "$d" 
     echo "START updating $d ..."
     git pull
+    echo "DONE. "
     echo " "
 done
